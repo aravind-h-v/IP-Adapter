9d250cef5b3b137e2f6837564e124d4f54b2a337
aravind <aravind@tri3d.in>
2024-02-27 15:04:36 +0530

    Made some changes to the full model

--- ip_adapter/__init__.py
+++ ip_adapter/__init__.py
@@ -1,4 +1,4 @@
-from .ip_adapter import IPAdapter, IPAdapterPlus, IPAdapterPlusXL, IPAdapterXL, IPAdapterFull
+from .ip_adapter import IPAdapter, IPAdapterPlus, IPAdapterPlusXL, IPAdapterXL, IPAdapterFull, IPAdapterFull_aravind
 
 __all__ = [
     "IPAdapter",
